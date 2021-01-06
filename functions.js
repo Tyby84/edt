cbf69ad5c9c209a3221e59f115db034cf0dd0f79
Tyby <tibofenyvesi84@gmail.com>
2021-01-06 13:39:12 +0100

    level1

---
+++ functions.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    $('.menu-bar').click(function() {
+        $('.nav__mobile').toggleClass('nav__mobile--shown');
+        $('.menu-bar').toggleClass('menu-bar--close');
+    });
+  
+  });
