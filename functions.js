8c7132d25c1547f5f9330c119129d816311d562e
Tyby <tibofenyvesi84@gmail.com>
2021-01-07 21:14:35 +0100

    level 2

--- functions.js
+++ functions.js
@@ -4,5 +4,30 @@ $(document).ready(function(){
         $('.nav__mobile').toggleClass('nav__mobile--shown');
         $('.menu-bar').toggleClass('menu-bar--close');
     });
-  
+    $('.egy').click(function() {
+        $('.table').css("display", "none");
+        $('#one').css("display", "block");
+        $('page-link').attr('href', '#egy').parent(".page-item").addClass('active');
+    });
+    $('.ketto').click(function() {
+        $('table').css("display", "none");
+        $('#two ').css("display", "block");
+        $('page-link').attr('href', '#ketto').parent(".page-item").addClass('active');
+    });
+    $('.harom').click(function() {
+        $('table').css("display", "none");
+        $('#three ').css("display", "block");
+    });
+    $('.negy').click(function() {
+        $('table').css("display", "none");
+        $('#four').css("display", "block");
+    });
+    $('.ot').click(function() {
+        $('table').css("display", "none");
+        $('#five').css("display", "block");
+    });
+    $('.hat').click(function() {
+        $('table').css("display", "none");
+        $('#six').css("display", "block");
+    });
   });
