c4538830349daed6ec0f48a73147d022427618df
Tyby <tibofenyvesi84@gmail.com>
2021-01-31 12:03:16 +0100

    refaktor

--- functions.js
+++ functions.js
@@ -1,47 +1,21 @@
-$(document).ready(function(){
-
+$(document).ready(function() {
+    /* Mobil menu megjelenites, zaras */ 
     $('.menu-bar').click(function() {
         $('.nav__mobile').toggleClass('nav__mobile--shown');
         $('.menu-bar').toggleClass('menu-bar--close');
     });
-    /* $('.egy').click(function() {
-        $('.table').css("display", "none");
-        $('#one').css("display", "block");
-        $('page-link').attr('href', '#egy').parent(".page-item").addClass('active');
-    });
-    $('.ketto').click(function() {
-        $('table').css("display", "none");
-        $('#two ').css("display", "block");
-        $('page-link').attr('href', '#ketto').parent(".page-item").addClass('active');
-    });
-    $('.harom').click(function() {
-        $('table').css("display", "none");
-        $('#three ').css("display", "block");
-    });
-    $('.negy').click(function() {
-        $('table').css("display", "none");
-        $('#four').css("display", "block");
-    });
-    $('.ot').click(function() {
-        $('table').css("display", "none");
-        $('#five').css("display", "block");
-    });
-    $('.hat').click(function() {
-        $('table').css("display", "none");
-        $('#six').css("display", "block");
-    }); */
-
+    /* Pagination mukodes */ 
     let link = $('.page-item');
+    let table = $('table');
     link.click(function() {
+        table.css("display", "none");
         let item = $(this).children("a").attr("id");
-        console.log(item);
-    });
+        $("table" + "#" + item).css("display", "block");
 
-    $('.page-item').click(function() {
-        $('.page-item').removeClass('active');
+        $(link).removeClass('active');
         $(this).addClass('active');
-    }); 
-    
+    });
+    /* Zaszlok - nemet, angol nyelvu bemutatkozas*/
     $('.german').click(function() {
         $('.nemetOldal').toggleClass('latszik');
     });
